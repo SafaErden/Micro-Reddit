aa5a0f163c4d5a57fbf499a7127313e6659b4e96
Christopher Amanor <kriskit7@gmail.com>
2020-06-12 13:16:05 +0000

    Add Comment model and migration

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 2 ,maximum: 20 }
+  validates :body, presence: true, length: { minimum: 2 ,maximum: 200 }
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :post_id, presence: true, numericality: { only_integer: true }
+end
