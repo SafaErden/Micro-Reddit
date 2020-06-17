c71d2b2ee0ed1e1164690f170edfcf17a2e6bd28
Christopher Amanor <kriskit7@gmail.com>
2020-06-17 07:53:02 +0000

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   validates :username, presence: true,
                        length: { minimum: 2, maximum: 12 }, uniqueness: true
   validates :email, presence: true, length: { minimum: 2, maximum: 12 },
-                   format: { with: URI::MailTo::EMAIL_REGEXP }
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :date_of_birth, presence: true, length: { minimum: 2, maximum: 12 }
 end
